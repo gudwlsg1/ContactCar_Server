c57308d14cd6359fbb5991e19cadc3691c68a0be
gudwlsg1 <33974894+gudwlsg1@users.noreply.github.com>
2019-04-18 15:49:51 +0900

    json 데이터 가져오기

---
+++ contactcar/database/migrations/2019_04_18_045443_create_buyers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuyersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buyers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 40)->nullable(false);
+            $table->string('userId' , 40)->nullable(false)->unique();
+            $table->string('email');
+            $table->string('password', 100)->nullable(false);
+            $table->string('address', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyers');
+    }
+}
--- contactcar/routes/web.php
+++ contactcar/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('buyers','BuyerController');
+
+#Route::post('/auth/buyers','BuyerController@store');
